e968e2af035d081ef203e60e968b288b1f6866d2
Damian Cygan <damian.cygan@microsoft.wsei.edu.pl>
2020-11-20 18:48:01 +0100

    phone moving, holes, ball, colors

--- cw4-ball/main.js
+++ cw4-ball/main.js
@@ -1,74 +1,123 @@
-const canv = document.querySelector('canvas');
-const ctx = canv.getContext('2d');
+const coords = [];
 
-const windowWd = window.innerWidth;
-const windowHg = window.innerHeight;
+window.addEventListener('DOMContentLoaded', (event) => {
+    const canv = document.querySelector('canvas');
+    const ctx = canv.getContext('2d');
 
+    const windowWd = window.innerWidth;
+    const windowHg = window.innerHeight;
 
-canv.width = windowWd;
-canv.height = windowHg;
+    const getRandomX = () => Math.floor(Math.random() * windowWd) + 10;
+    const getRandomY = () => Math.floor(Math.random() * windowHg) + 10;
+    const colors = ['red', 'red', 'red','red', 'red', 'red', 'green'];
 
-const hole = {
-    x: 0,
-    y: 0,
-    r: 20,
-}
+    colors.forEach((color) => {
+        coords.push({
+            x: getRandomX(),
+            y: getRandomY(),
+            color,
+        });
+    });
 
-const ball = {
-    x: 0,
-    y: 0,
-    r: 10,
-    speedX: 1,
-    speedY: 1
-}
+    canv.width = windowWd;
+    canv.height = windowHg;
 
-ball.x = windowWd/2 - ball.r;
-ball.y = windowHg/2 - ball.r;
+    var maxX = canv.clientWidth;
+    var maxY = canv.clientHeight;
 
-let randomX = Math.floor(Math.random() * windowWd) + 10;
-let randomY = Math.floor(Math.random() * windowHg) + 10;
-
-
-function drawTable(){
-    ctx.fillStyle = "coral";
-    ctx.fillRect(0,0,windowWd, windowHg);
-}
-
-
-function drawHole(){
-    ctx.fillStyle = 'black';
-    ctx.fillRect(randomX-hole.r, randomY-hole.r, hole.r*2, hole.r*2);
-}
-
-function drawBall(){
-    // ctx.fillRect(windowWd/2 - ball.r, windowHg/2-ball.r,ball.r*2, ball.r*2)
-    // ctx.fillRect(ball.x, ball.y, ball.r*2, ball.r*2 );
-    ctx.beginPath();
-    ctx.arc(ball.x, ball.y, ball.r*2,0, 2 * Math.PI);
-    ctx.stroke();
-
-    if(ball.y <= 0 + ball.r || ball.y >= windowHg - ball.r*2){
-         ball.speedY = -ball.speedY;
+    const hole = {
+        x: 0,
+        y: 0,
+        r: 20,
     }
-    if(ball.x<=0 + ball.r || ball.x >= windowWd - ball.r*2){
-        ball.speedX = -ball.speedX;
+
+    const ball = {
+        x: 0,
+        y: 0,
+        r: 10,
+        speedX: 1,
+        speedY: 1
     }
-}
 
-function handleOrientation(e){
-    console.log(e.alpha + ' : ' + e.beta + ' : ' + e.gamma);
-    ball.speedX = e.gamma;
-    ball.speedY = e.beta; 
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
-}
+    ball.x = windowWd / 2 - ball.r;
+    ball.y = windowHg / 2 - ball.r;
 
-window.addEventListener("deviceorientation", handleOrientation, true);
+    // let randomX = Math.floor(Math.random() * windowWd) + 10;
+    // let randomY = Math.floor(Math.random() * windowHg) + 10;
 
-function game(){
-    drawTable();
-    drawHole();
-    drawBall();
-}
 
-setInterval(game,1000/60) //60fps
+    function drawTable() {
+        ctx.fillStyle = "coral";
+        ctx.fillRect(0, 0, windowWd, windowHg);
+    }
+
+
+    function drawHole(color = "green", x,y) {
+        ctx.fillStyle = color;
+
+        ctx.beginPath();
+        ctx.arc(x, y, hole.r * 2, 0, 2 * Math.PI)
+        ctx.fill();
+        // ctx.closePath();
+    }
+
+    function drawBall(e) {
+        ctx.fillStyle = "black"
+        ctx.beginPath();
+        ctx.arc(ball.x, ball.y, ball.r * 2, 0, 2 * Math.PI);
+        ctx.fill();
+        ctx.closePath();
+
+
+
+        ball.x += ball.speedX;
+        ball.y += ball.speedY;
+
+        if (ball.y <= 0 + ball.r || ball.y >= windowHg - ball.r * 2) {
+            ball.speedY = -ball.speedY;
+        }
+        if (ball.x <= 0 + ball.r || ball.x >= windowWd - ball.r * 2) {
+            ball.speedX = -ball.speedX;
+        }
+    }
+
+    function handleOrientation(event){
+        console.log("handleOrientation -> event", event);
+        var x = event.beta;  // In degree in the range [-180,180]
+        var y = event.gamma; // In degree in the range [-90,90]
+
+        // Because we don't want to have the device upside down
+        // We constrain the x value to the range [-90,90]
+        if (x >  90) { x =  90};
+        if (x < -90) { x = -90};
+
+        // To make computation easier we shift the range of
+        // x and y to [0,180]
+        x += 90;
+        y += 90;
+
+        // 10 is half the size of the ball
+        // It center the positioning point to the center of the ball
+        console.log("handleOrientation -> (maxY * y / 180 - 10)", (maxY * y / 180 - 10));
+        console.log("handleOrientation -> (maxX * x / 180 - 10)", (maxX * x / 180 - 10));
+        ball.x = (maxY * y / 180 - 10);
+        ball.y = (maxX * x / 180 - 10);
+    }
+
+    window.addEventListener("deviceorientation", handleOrientation, true);
+
+
+    coords.forEach(({ x, y, color }) => {
+        drawHole(color, x, y);
+    })
+
+    function game() {
+        drawTable();
+        coords.forEach(({ x, y, color }) => {
+            drawHole(color, x, y);
+        })
+        drawBall();
+    }
+
+    setInterval(game, 1000 / 60) //60fps
+});
