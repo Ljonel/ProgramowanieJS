d7f28bfdc66c357f02bd89c795095a723f96a3d2
Damian Cygan <damian.cygan@microsoft.wsei.edu.pl>
2021-01-13 18:33:23 +0100

    weather oop

--- cw1-lightbox/main.js
+++ cw1-lightbox/main.js
@@ -1,34 +1,42 @@
 let gallery = document.querySelectorAll('.gallery img');
-const lightbox = document.querySelector('.lightbox');
-const lightboxImg = document.querySelector('.lightbox img');
-const prev = document.querySelector('.prev')
-const next = document.querySelector('.next')
 
-gallery.forEach(element => {
-    element.addEventListener('click', function(){
+
+gallery.forEach((element) => {
+    element.addEventListener('click', function () {
+        const lightbox = document.querySelector('.lightbox');
+        const lightboxImg = document.querySelector('.lightbox img');
+        const prev = document.querySelector('.prev')
+        const next = document.querySelector('.next')
         lightboxImg.src = element.src;
         lightbox.classList.add('visible');
 
-        next.addEventListener('click', () =>{
-            if(element.nextElementSibling != null){
+        next.addEventListener('click', () => {
+            if (element.nextElementSibling != null) {
                 lightboxImg.src = element.nextElementSibling.src;
                 element = element.nextElementSibling;
+            } else {
+                console.log("koniec zdjec")
             }
         })
-
-        prev.addEventListener('click', () =>{
-            if(element.previousElementSibling != null){
+        prev.addEventListener('click', () => {
+            if (element.previousElementSibling != null) {
                 lightboxImg.src = element.previousElementSibling.src;
                 element = element.previousElementSibling;
+            } else {
+                console.log("koniec zdjec")
             }
         })
+
+
+        lightbox.addEventListener('click', function (e) {
+            if (e.target !== e.currentTarget) {
+                return
+            }
+            lightbox.classList.remove('visible');
+        })
     })
+
+
+
+
 });
-
-
-function hideLightbox(e){
-    if(e.target !== e.currentTarget) return;
-    lightbox.classList.remove('visible');
-}
-
-lightbox.addEventListener('click', hideLightbox)
